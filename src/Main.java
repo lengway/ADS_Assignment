897039c0c3283a1b0ad6b983f5d96e972d9942f2
lengway <lengway@inbox.ru>
2025-05-24 09:16:18 +0500

    first

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WeightedGraph<String> graph = new WeightedGraph<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Vertex<String> a = graph.addVertex("A");
+        Vertex<String> b = graph.addVertex("B");
+        Vertex<String> c = graph.addVertex("C");
+        Vertex<String> d = graph.addVertex("D");
+
+        graph.addEdge(a, b, 1.0);
+        graph.addEdge(a, c, 4.0);
+        graph.addEdge(b, c, 2.0);
+        graph.addEdge(b, d, 5.0);
+        graph.addEdge(c, d, 1.0);
+
+        // BFS (unweighted shortest path in hops)
+        BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(graph);
+        bfs.search(a);
+        System.out.println("BFS distance (in hops) from A to D: " + bfs.getDistance(d));
+        System.out.println("Path: " + bfs.getPath(d));
+
+        // Dijkstra (weighted shortest path)
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(graph);
+        dijkstra.search(a);
+        System.out.println("Dijkstra distance from A to D: " + dijkstra.getDistance(d));
+        System.out.println("Path: " + dijkstra.getPath(d));
     }
 }
